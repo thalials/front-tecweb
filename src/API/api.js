b91c996ea192607cf5e2220f974aa74b7adaf63e
Thalia Loiola <thalials@al.insper.edu.br>
2020-11-09 19:49:32 -0300

    ajuste para deploy

--- src/API/api.js
+++ src/API/api.js
@@ -1,5 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
+
+const current_URL = window.location.href;
 
 export default axios.create({
-    baseURL: 'http://localhost:3333' // baseurl do backend
+//   baseURL: current_URL.startsWith("http://localhost") ? "http://localhost:3333" : "https://tecweb-p2.herokuapp.com/" // baseurl do backend
+    baseURL: process.env.REACT_APP_APP_API_URL
 });
